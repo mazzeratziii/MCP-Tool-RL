93d467218431d226379035a93401067a9dc5f7b4
mazzeratzi <grauxxx1@yandex.ru>
2026-01-15 20:04:28 +0500

    первый трай

--- tools/base_tool.py
+++ tools/base_tool.py
@@ -1,9 +1,7 @@
-from abc import ABC, abstractmethod
-class BaseTool(ABC):
+class BaseTool:
     def __init__(self, name:str, description:str ):
         self.name = name
         self.description = description
 
-    @abstractmethod
     def invoke(self, query:str) -> str:
         pass
